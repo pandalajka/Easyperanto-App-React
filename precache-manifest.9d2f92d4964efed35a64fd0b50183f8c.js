4846a46105311edb845e335b6188866187913e81
PANDALAJKA <KURZAWA.PATRYCJA@GMAIL.COM>
2018-10-18 01:50:36 +0200

    Updates

---
+++ precache-manifest.9d2f92d4964efed35a64fd0b50183f8c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8f100ac351640be3d9676dd9a0514c9",
+    "url": "/Easyperanto-App-React/static/media/undraw_online_world_mc1t.c8f100ac.svg"
+  },
+  {
+    "revision": "ec9e6828e5bd6ca82b7740e18ba2e387",
+    "url": "/Easyperanto-App-React/static/media/undraw_on_the_office_fbfs.ec9e6828.svg"
+  },
+  {
+    "revision": "573428065e77e546241d32aede3e7528",
+    "url": "/Easyperanto-App-React/static/media/undraw_404.57342806.svg"
+  },
+  {
+    "revision": "30129480e943e5b3e334394168ae16a2",
+    "url": "/Easyperanto-App-React/static/media/app-icon2.30129480.png"
+  },
+  {
+    "revision": "f0bb831e54f16acc3c8b",
+    "url": "/Easyperanto-App-React/static/js/runtime~main.f0bb831e.js"
+  },
+  {
+    "revision": "5fbd4437f2e73845348b",
+    "url": "/Easyperanto-App-React/static/js/main.5fbd4437.chunk.js"
+  },
+  {
+    "revision": "e571561953a635172dc6",
+    "url": "/Easyperanto-App-React/static/js/1.e5715619.chunk.js"
+  },
+  {
+    "revision": "5fbd4437f2e73845348b",
+    "url": "/Easyperanto-App-React/static/css/main.bac0f3ee.chunk.css"
+  },
+  {
+    "revision": "e571561953a635172dc6",
+    "url": "/Easyperanto-App-React/static/css/1.8c5ace65.chunk.css"
+  },
+  {
+    "revision": "1964594019666b136b78486f3345de78",
+    "url": "/Easyperanto-App-React/index.html"
+  }
+];
